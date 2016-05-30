507ddbfbc49b3273c5d064267ac1d10208be34de
Tris Setiawan <tris.setiawan@yahoo.com>
2016-05-30 13:59:01 +0700

    1.0.3
    expose header:Content-Disposition

--- src/middlewares/enable-cors.js
+++ src/middlewares/enable-cors.js
@@ -2,6 +2,7 @@ module.exports = function (request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token, Authorization");
+    response.header("Access-Control-Expose-Headers", "Content-Disposition");
     if ('OPTIONS' == request.method) {
         response.sendStatus(204);
     }
